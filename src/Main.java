2c419f21bcad3991ecb2a23a61fdfe86c7f7c41b
Hamadi Hachemi <ham.bicho@gmail.com>
2016-04-05 09:50:51 +0100

    Login interface is now fully working

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,23 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ui/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        BorderPane borderPane = new BorderPane();
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("ui/style/style_login.css").toExternalForm());
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Controller {
+    @FXML
+    private javafx.scene.control.Button btnClose;
+    @FXML private javafx.scene.control.ToolBar toolBar;
+    private double xOffset;
+    private double yOffset;
+
+
+    @FXML private void setOnMousePressed(MouseEvent event){
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+    }
+
+    @FXML private void setOnMouseDragged(MouseEvent event){
+        Stage stage = (Stage) toolBar.getScene().getWindow();
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset );
+    }
+    @FXML
+    private void closeButtonAction(){
+        // get a handle to the stage
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+    }
 }
