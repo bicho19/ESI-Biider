819f2a21868c34eec80a7109d2c62bb137f45f97
Hamadi Hachemi <ham.bicho@gmail.com>
2016-04-05 10:02:27 +0100

    Login interface is now fully working, and folders are organized

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/sample.fxml"));
         BorderPane borderPane = new BorderPane();
 
         Scene scene = new Scene(root);
