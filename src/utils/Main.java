d6bf5b2ef370547beaf5916619dd470ec64d350c
Hamadi Hachemi <ham.bicho@gmail.com>
2016-04-20 22:11:42 +0100

    Finished working on the webccam driver, it works fine, needs few more style.

--- src/utils/Main.java
+++ src/utils/Main.java
@@ -1,14 +1,38 @@
 package utils;
 
+import com.github.sarxos.webcam.Webcam;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.awt.image.BufferedImage;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main extends Application {
     public static Parent getRoot() {
@@ -32,6 +56,7 @@ public class Main extends Application {
     private static Stage stage;
 
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         root = FXMLLoader.load(getClass().getResource("/ui/login.fxml"));
@@ -48,6 +73,7 @@ public class Main extends Application {
 
         stage = primaryStage;
     }
+
 
 
     public static void main(String[] args) {
