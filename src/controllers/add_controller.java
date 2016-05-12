c60c99c6b4a2ed3080ac748599785448c8810474
Hamadi Hachemi <ham.bicho@gmail.com>
2016-05-12 17:08:46 +0100

    Add almost half work

--- src/controllers/add_controller.java
+++ src/controllers/add_controller.java
@@ -1,42 +1,57 @@
 package controllers;
 
+import com.jfoenix.controls.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.File;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
 
 /**c
  * Created by Creator on 11/05/2016.
  */
-public class add_controller extends communs{
-    @FXML TextField ID = new TextField();
-    @FXML TextField Nom = new TextField();
-    @FXML TextField Prenom = new TextField();
-    @FXML TextField Date = new TextField();
-    @FXML TextField Lieu = new TextField();
-    @FXML TextField Adresse = new TextField();
-    @FXML Button btnCapture = new Button();
+public class add_controller extends communs implements Initializable{
+    @FXML JFXTextField ID = new JFXTextField();
+    @FXML JFXTextField fName = new JFXTextField();
+    @FXML JFXTextField lName = new JFXTextField();
+    @FXML JFXDatePicker Date = new JFXDatePicker();
+    @FXML JFXTextField bPlace = new JFXTextField();
+    @FXML JFXTextField Address = new JFXTextField();
+    @FXML JFXComboBox<String> Sex = new JFXComboBox<>();
+    @FXML JFXButton btnCapture = new JFXButton();
+    @FXML JFXButton btnLoad = new JFXButton();
+    @FXML JFXButton btnAdd = new JFXButton();
 
     // On "Ajouter" button click
     @FXML
-    private void btnAdd(){
+    public void onAddClick(){
+        LocalDate localDate = Date.getValue();
+        String sex = Sex.getSelectionModel().getSelectedItem();
+
+        if (ID.getText().equals("") || fName.getText().equals("") || lName.getText().equals("") ||
+                Date.getValue() == null || bPlace.getText().equals("") || Address.getText().equals("") || Sex.getSelectionModel().getSelectedItem().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR,"Please fill all the information");
+                    alert.showAndWait()
+                    .filter(response -> response == ButtonType.OK);        }
+
         // Test the fields before add to the database
-        if(ID.getText().equals("") || Nom.getText().equals("") || Prenom.getText().equals("") || Date.getText().equals("")
-                || Lieu.getText().equals("") || Adresse.getText().equals("")){
-            // Show error message if it didn't work
-            System.out.println("You should give all the information");
-        }else {
-            // Make the query go ....
-            System.out.println("... Like A Boss ...");
-        }
+
     }
     @FXML public void onCaptureClick(ActionEvent actionEvent) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/ui/layouts/popups/webcam.fxml"));
@@ -58,4 +73,14 @@ public class add_controller extends communs{
         String path = file.getPath();
         System.out.println(path);
     }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> options = FXCollections.observableArrayList();
+        options.addAll("Male","Female");
+        Sex.setItems(options);
+    }
+
+
+
 }
